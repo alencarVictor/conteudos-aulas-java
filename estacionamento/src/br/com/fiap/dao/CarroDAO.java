9c977648fa77dab80bd31529ae5702bb6e007467
Victor Alencar <alencarfelkipe@gmail.com>
2026-08-25 23:24:00 -0300

    adiciona projeto de introdução a conexao bd

---
+++ estacionamento/src/br/com/fiap/dao/CarroDAO.java
@@ -0,0 +1,41 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.dto.Carro;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CarroDAO {
+    //import
+    private Connection con;
+
+    //construtor com passagem de parametro
+    public CarroDAO(Connection con) {
+        this.con = con;
+    }
+
+    //getter
+    public Connection getCon() {return con;}
+
+    //metodo da classe
+    public String inserir(Carro carro){
+        String sql = "insert into ddd_carro(placa,cor,descricao) values(?,?,?)";
+        //try-with-resources
+        try (PreparedStatement ps =  getCon().prepareStatement(sql)){
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getCor());
+            ps.setString(3, carro.getDescricao());
+            if (ps.executeUpdate() > 0){
+                return "Inserido com sucesso";
+
+            }else {
+                return "Erro ao inserir";
+
+            }
+
+        }catch (SQLException e){
+            return "Erro de SQL:" + e.getMessage();
+        }
+    }
+}
